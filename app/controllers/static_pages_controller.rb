9e8195ac706b2df4f04395122c275600a7cc70a8
cyanobacteria <vodomelkor@gmail.com>
2017-07-15 08:42:51 +0800

    params problem

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,28 @@
 class StaticPagesController < ApplicationController
+  before_action :set_post, only: [:only_view_show_one]
   def home
   end
 
   def about
   end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-
+  resources :static_pages
   resources :posts
   devise_for :admins
-  root 'static_pages#home'
+  root 'static_pages#index'
  #get  'static_pages/home'
-
+  get  'static_pages/only_view_show_all'
   get  'static_pages/about'
+ #get  'static_pages/only_view_show_one/:id'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'users#index'
